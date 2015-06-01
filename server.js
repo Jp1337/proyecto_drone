5b7133bc9fa6402552552fa934bc6de50f67ebad
Juan Pablo Alvarez <juan.alvareza@mail.udp.cl>
2015-05-31 23:49:22 -0300

    Descripción del uso

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-//emular envio de frames
+// Emular envio de frames
+// Esto lee frames de una carpeta "sample_data",
+// Para poder trabajar sin un drone
+
 var http    = require('http');
 var fs		= require('fs');
-
-var buff = new Buffer(262114);	//8388608);
 var lastPng;
 var base = 100051;
 var frms = 100127 - base;
 var count = 1;
-
 var server = http.createServer(function(req, res) {
-
   res.writeHead(200, {'Content-Type': 'image/png'});
   res.writeContinue();
-  res.end(fs.readFileSync("sample_data/"+(base+(count%frms))+".png")); //llenar con .png de un video esta carpeta
+  res.end(fs.readFileSync("sample_data/"+(base+(count%frms))+".png")); 
   console.log('Sirviendo imagen n°'+(base+(count%frms))+'.');
   count = count + 1;
-  
 });
 
 server.listen(8080, function() {
